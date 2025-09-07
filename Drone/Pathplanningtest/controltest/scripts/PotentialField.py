e4ac563edc8378a5ce365c444e0cde29be29eb4e
TNTTNTTTN <isugkgkgk@gmail.com>
2025-09-08 03:01:46 +0900

    Revise File Structure

---
+++ Drone/Pathplanningtest/controltest/scripts/PotentialField.py
@@ -0,0 +1,444 @@
+#!/usr/bin/env python3
+
+PKG = 'px4'
+import sys
+import rospy
+import math
+import numpy as np
+import pyproj
+import scipy.spatial.transform
+from geometry_msgs.msg import PoseStamped, Quaternion, Vector3, Twist
+from mavros_msgs.msg import ParamValue, AttitudeTarget, PositionTarget, LandingTarget
+from mavrosinit import Mavrosinit
+from darknetinit import Darknetinit
+from pymavlink import mavutil
+from six.moves import xrange
+from std_msgs.msg import Header
+from threading import Thread
+# from tf.transformations import quaternion_from_euler
+
+class Ctrtest(Darknetinit, Mavrosinit):
+    def __init__(self, systype, wp1lat=0 , wp1lon=0, wp2lat=0, wp2lon=0, wp3lat=0, wp3lon=0):
+        self.wp1lat = float(wp1lat)
+        self.wp1lon = float(wp1lon)
+        self.wp2lat = float(wp2lat)
+        self.wp2lon = float(wp2lon)
+        self.wp3lat = float(wp3lat)
+        self.wp3lon = float(wp3lon)
+        super(Ctrtest, self).__init__()
+        if systype == 1:
+        # Posistion control setup
+            self.pos = PoseStamped()
+            self.radius = 1
+
+            self.pos_setpoint_pub = rospy.Publisher(
+                'mavros/setpoint_position/local', PoseStamped, queue_size=1)
+
+            # send setpoints in seperate thread to better prevent failsafe
+            self.pos_thread = Thread(target=self.send_pos, args=())
+            self.pos_thread.daemon = True
+            self.pos_thread.start()
+
+        elif systype == 2:
+            # Attitude control setup
+            self.att = AttitudeTarget()
+
+            self.att_setpoint_pub = rospy.Publisher(
+                'mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=1)
+
+            # send setpoints in seperate thread to better prevent failsafe
+            self.att_thread = Thread(target=self.send_att, args=())
+            self.att_thread.daemon = True
+            self.att_thread.start()
+
+        elif systype == 3:
+            self.vel = PositionTarget()
+            self.vel.velocity = Vector3()
+            self.radius = 0.5
+
+            self.vel_setpoint_pub = rospy.Publisher(
+                'mavros/setpoint_raw/local', PositionTarget, queue_size=1)
+            self.vel_thread = Thread(target=self.send_vel, args=())
+            self.vel_thread.daemon = True
+            self.vel_thread.start()
+        elif systype == 4:
+            self.vel_ang = Twist()
+            self.vel_ang.linear = Vector3()
+            self.vel_ang.angular = Vector3()
+            self.vel_ang_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel_unstamped'
+                                               , Twist, queue_size=1)
+            self.vel_ang_thread = Thread(target=self.send_vel_ang, args=())
+            self.vel_ang_thread.daemon = True
+            self.vel_ang_thread.start()
+    def send_pos(self):
+        rate = rospy.Rate(10)  # Hz
+        self.pos.header = Header()
+        self.pos.header.frame_id = "base_footprint"
+
+        while not rospy.is_shutdown():
+            self.pos.header.stamp = rospy.Time.now()
+            self.pos_setpoint_pub.publish(self.pos)
+            try:  # prevent garbage in console output when thread is killed
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                pass
+
+    def send_att(self):
+        rate = rospy.Rate(10)  # Hz
+        self.att.body_rate = Vector3()
+        self.att.header = Header()
+        self.att.header.frame_id = "base_footprint"
+        # self.att.orientation = Quaternion(*quaternion_from_euler(0, 0, 0, axes='rzyx'))
+        # self.att.type_mask = 7  # ignore body rate
+        self.att.type_mask = 192
+        while not rospy.is_shutdown():
+            self.att.header.stamp = rospy.Time.now()
+            self.att_setpoint_pub.publish(self.att)
+            try:  # prevent garbage in console output when thread is killed
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                pass
+
+    def send_vel(self):
+        rate = rospy.Rate(10)  # Hz
+        self.vel.header = Header()
+        self.vel.header.frame_id = "base_footprint"
+        self.vel.coordinate_frame = 1 # Local NED
+        self.vel.type_mask = 7 # Ignore Body position
+
+        while not rospy.is_shutdown():
+            self.vel.header.stamp = rospy.Time.now()
+            self.vel_setpoint_pub.publish(self.vel)
+
+            try:  # prevent garbage in console output when thread is killed
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                pass
+    def send_vel_ang(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.vel_ang_pub.publish(self.vel_ang)
+
+            try:  # prevent garbage in console output when thread is killed
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                pass
+
+    def lla2enu(self,lat, lon, alt, lat_org, lon_org, alt_org):
+        transformer = pyproj.Transformer.from_crs(
+            {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
+            {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
+        )
+        x, y, z = transformer.transform(lon, lat, alt, radians=False)
+        x_org, y_org, z_org = transformer.transform(lon_org, lat_org, alt_org, radians=False)
+        vec = np.array([[x - x_org, y - y_org, z - z_org]]).T
+
+        rot1 = scipy.spatial.transform.Rotation.from_euler('x', -(90 - lat_org),
+                                                           degrees=True).as_matrix()  # angle*-1 : left handed *-1
+        rot3 = scipy.spatial.transform.Rotation.from_euler('z', -(90 + lon_org),
+                                                           degrees=True).as_matrix()  # angle*-1 : left handed *-1
+
+        rotMatrix = rot1.dot(rot3)
+
+        enu = rotMatrix.dot(vec).T.ravel()
+        return enu.T
+
+    def wptsetup(self):
+        initlat = float(self.global_position.latitude)
+        initlon = float(self.global_position.longitude)
+        inithight = float(self.global_position.altitude)
+        wp1 = self.lla2enu(self.wp1lat, self.wp1lon, inithight+15, initlat, initlon, inithight)
+        wp2 = self.lla2enu(self.wp2lat, self.wp2lon, inithight+15, initlat, initlon, inithight)
+        wp3 = self.lla2enu(self.wp3lat, self.wp3lon, inithight+15, initlat, initlon, inithight)
+        self.going = ((wp1[0], wp1[1], wp1[2]), (wp2[0], wp2[1], wp2[2]), (wp3[0], wp3[1], wp3[2]))
+        self.back = ((wp3[0], wp3[1], wp1[2]), (wp2[0], wp2[1], wp2[2]), (wp1[0], wp1[1], wp1[2]), (0, 0, 5))
+
+    def is_at_position(self, x, y, z, offset):
+        """offset: meters"""
+        rospy.logdebug(
+            "current position | x:{0:.2f}, y:{1:.2f}, z:{2:.2f}".format(
+                self.local_position.pose.position.x, self.local_position.pose.
+                position.y, self.local_position.pose.position.z))
+
+        desired = np.array((x, y, z))
+        pos = np.array((self.local_position.pose.position.x,
+                        self.local_position.pose.position.y,
+                        self.local_position.pose.position.z))
+        return np.linalg.norm(desired - pos) < offset
+
+    def reach_position(self, x, y, z, timeout):
+        """timeout(int): seconds"""
+        # set a position setpoint
+        self.pos.pose.position.x = x
+        self.pos.pose.position.y = y
+        self.pos.pose.position.z = z
+        rospy.loginfo(
+            "attempting to reach position | x: {0}, y: {1}, z: {2} | current position x: {3:.2f}, y: {4:.2f}, z: {5:.2f}".
+            format(x, y, z, self.local_position.pose.position.x,
+                   self.local_position.pose.position.y,
+                   self.local_position.pose.position.z))
+        rospy.loginfo("global position checking | lat: {0}, lon: {1}, alt1:{2}, alt2:{3}"
+                      .format(self.global_position.latitude,
+                              self.global_position.longitude,
+                              self.altitude.local, self.global_position.altitude))
+
+        # For demo purposes we will lock yaw/heading to north.
+        headingy = y - self.local_position.pose.position.y
+        headingx = x - self.local_position.pose.position.x
+        yaw = math.atan2(headingy,headingx)
+        # quaternion = quaternion_from_euler(yaw, 0, 0, axes="rzyx")
+        # self.pos.pose.orientation = Quaternion(*quaternion)
+
+        # does it reach the position in 'timeout' seconds?
+        loop_freq = 2  # Hz
+        rate = rospy.Rate(loop_freq)
+        for i in xrange(timeout * loop_freq):
+            if self.is_at_position(self.pos.pose.position.x,
+                                   self.pos.pose.position.y,
+                                   self.pos.pose.position.z, self.radius):
+                rospy.loginfo("position reached | seconds: {0} of {1}".format(
+                    i / loop_freq, timeout))
+                break
+
+            try:
+                rate.sleep()
+            except rospy.ROSException:
+                quit()
+
+    def potentialflow(self,ori_x, ori_y, ori_z, x, y, z):
+        """timeout(int): seconds"""
+        currentx = self.local_position.pose.position.x
+        currenty = self.local_position.pose.position.y
+        currentz = self.local_position.pose.position.z
+        posdiff = np.sqrt((x - currentx)**2 + (y-currenty)**2)
+        directionvec = [x - ori_x, y - ori_y]
+        if posdiff > 30:
+            if self.setupspeed < 10:
+                self.setupspeed += 0.2
+            else:
+                self.setupspeed = 10
+        else :
+            if self.setupspeed > 0.25 * posdiff:
+                self.setupspeed -= 0.2
+            else :
+                self.setupspeed = 0.25 * posdiff
+
+        alpha = math.atan2(y - ori_y, x - ori_x)
+        yaw = math.atan2(y - currenty, x - currentx)
+        vector = self.uniformveccal(self.setupspeed, alpha) + self.sinkveccal(currentx,currenty,x,y,600)
+        if np.dot(directionvec, [currentx - x, currenty - y]) >= 0:
+            vector = np.array([x - currentx, y - currenty])
+        elif hasattr(self, 'Obj'):
+            for i in range(len(self.Obj)):
+                if i == 0:
+                    continue
+                elif np.dot(directionvec,[self.Obj[i][0] - x, self.Obj[i][1] - y]) >= 0:
+                    continue
+                elif abs(self.uniformpocal(currentx,currenty,alpha,self.setupspeed) - self.uniformpocal(self.Obj[i][0], self.Obj[i][1], alpha, self.setupspeed)) < self.setupspeed:
+                    if currenty - self.Obj[i][1] == 0:
+                        vector[1] += self.setupspeed
+                    else:
+                        vector[1] += 15*(currenty - self.Obj[i][1])/abs((currenty - self.Obj[i][1]))
+                vector += self.doublitveccal(currentx, currenty, self.Obj[i][0], self.Obj[i][1], self.Obj[i][2], self.setupspeed, alpha)
+                rospy.loginfo("{}, {}".format(i, self.doublitveccal(currentx, currenty, self.Obj[i][0], self.Obj[i][1], self.Obj[i][2], self.setupspeed, alpha)))
+        vector = vector/np.linalg.norm(vector)
+        self.vel.velocity.x = self.setupspeed * vector[0]
+        self.vel.velocity.y = self.setupspeed * vector[1]
+        self.vel.velocity.z = 15 - currentz
+        self.vel.yaw = yaw
+        # self.vel.acceleration_or_force.x = 1 * vector[0]
+        # self.vel.acceleration_or_force.y = 1 * vector[1]
+        rospy.loginfo("velocity: {0}, {1}, {2}".format(round(self.vel.velocity.x, 2), round(self.vel.velocity.y, 2), round(self.vel.velocity.z, 2)))
+        rospy.loginfo("position: {0}, {1}".format(round(self.local_position.pose.position.x, 2),round(self.local_position.pose.position.y, 2)))
+        rospy.loginfo("target_velocity: {0}".format(self.local_vel.twist.linear))
+    def uniformpocal(self, x, y, alpha, speed):
+        return speed*(y * math.cos(alpha) - x * math.sin(alpha))
+
+    def uniformveccal(self, speed, alpha):
+        return np.array([speed*math.cos(alpha), speed*math.sin(alpha)])
+
+    def doublitpocal(self, x, y, objx, objy, range, speed):
+        return -speed*(range ** 2)*(y-objy)/((x-objx) ** 2 + (y-objy) ** 2)
+
+    def doublitveccal(self, x, y, objx, objy, range, speed, alpha):
+        vecx = -speed*(range**2)*((x-objx)**2 - (y-objy)**2)/(((x-objx)**2 + (y-objy)**2)**2)
+        vecy = -2*speed*(range**2)*((x-objx)*(y-objy))/(((x-objx)**2 + (y-objy)**2)**2)
+        return np.array([vecx*math.cos(alpha) + vecy*math.sin(alpha), vecx*math.sin(alpha) + vecy*math.cos(alpha)])
+
+    def sinkpocal(self, x, y, posx, posy, strength):
+        return strength*math.atan2(y-posy,x-posx)
+
+    def sinkveccal(self,x, y, posx, posy, strength):
+        vecx = -strength * (x-posx)/((x-posx)**2 + (y-posy)**2)
+        vecy = -strength * (y-posy)/((x-posx)**2 + (y-posy)**2)
+        return np.array([vecx, vecy])
+
+    def precisionlanding(self):
+        self.landing_target_sub = rospy.Subscriber('mavros/landing_target/raw', LandingTarget,
+                                            self.landingtarget_callback)
+        self.landing_target = LandingTarget()
+        self.set_mode('AUTO.PRECLAND',5)
+
+    def test_posctl(self):
+        """Test offboard position control"""
+
+        # make sure the simulation is ready to start the mission
+        self.wait_for_topics(60)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   10, -1)
+
+        self.log_topic_vars()
+        # exempting failsafe from lost RC to allow offboard
+        rcl_except = ParamValue(1<<2, 0.0)
+        self.set_param("COM_RCL_EXCEPT", rcl_except, 5)
+        self.set_arm(True, 5)
+        self.set_mode("OFFBOARD", 5)
+        rospy.loginfo("run mission")
+        # positions = ((0, 0, 15), (-100, 55, 15), (-180, 99, 15), (-100, 50, 15),
+        #              (0, 0, 15), (0, 0, 3))
+        positions = ((0, 0, 1), (0, 0,10))
+
+        for i in xrange(len(positions)):
+            self.reach_position(positions[i][0], positions[i][1],
+                                positions[i][2], 20)
+
+        # self.set_mode("AUTO.LAND", 5)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   45, 0)
+        self.set_arm(False, 5)
+
+    def test_attctl(self):
+        """Test offboard attitude control"""
+        # make sure the simulation is ready to start the mission
+        self.wait_for_topics(60)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   10, -1)
+
+        self.log_topic_vars()
+        self.wptsetup()
+        self.set_mode("OFFBOARD", 5)
+        self.set_arm(True, 5)
+        rospy.loginfo("run mission")
+        for i in range(len(self.going)):
+            self.reach_position(self.going[i][0], self.going[i][1],
+                                self.going[i][2], 30)
+
+        for j in range(len(self.back)):
+            self.reach_position(self.back[j][0], self.back[j][1],
+                                self.back[j][2], 30)
+
+        # does it cross expected boundaries in 'timeout' seconds?
+        self.set_mode("AUTO.LAND", 5)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   90, 0)
+        self.set_arm(False, 5)
+
+    def test_vel(self):
+        self.wait_for_topics(60)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   10, -1)
+
+        self.log_topic_vars()
+        self.wptsetup()
+        self.set_mode("OFFBOARD", 5)
+        self.set_arm(True, 5)
+        position = ((0,0,0), (0,0,15), (100,0,15), (100,100,15), (0, 0, 15))
+        self.Obj = ((),(25,3,20), (50,-5,15), (100,73,20), (50,50,20))
+        pos = 1
+        timeout = 180
+        loop_freq = 10  # Hz
+        rate = rospy.Rate(loop_freq)
+        self.setupspeed = 0
+        for i in xrange(timeout * loop_freq):
+            if pos == len(position):
+                break
+            self.potentialflow(position[pos-1][0], position[pos-1][1], position[pos-1][2], position[pos][0], position[pos][1], position[pos][2])
+            if self.is_at_position(position[pos][0], position[pos][1], self.local_position.pose.position.z,self.radius):
+                rospy.loginfo("position reached!")
+                pos += 1
+
+            try:
+                rate.sleep()
+            except rospy.ROSException:
+                quit()
+        self.precisionlanding()
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   90, 0)
+        self.set_arm(False, 5)
+    def test_vel_ang(self):
+        self.wait_for_topics(60)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   10, -1)
+
+        self.log_topic_vars()
+        self.set_mode("OFFBOARD", 5)
+        self.set_arm(True, 5)
+        timeout = 10
+        loop_freq = 1  # Hz
+        rate = rospy.Rate(loop_freq)
+        for i in xrange(timeout * loop_freq):
+            self.vel_ang.linear.x = 0
+            self.vel_ang.linear.y = 0
+            self.vel_ang.linear.z = 2
+            self.vel_ang.angular.x = 0
+            self.vel_ang.angular.y = 0
+            self.vel_ang.angular.z = 0
+            try:
+                rate.sleep()
+            except rospy.ROSException:
+                quit()
+        for j in xrange(timeout-5 * loop_freq):
+            self.vel_ang.linear.x = 1
+            self.vel_ang.linear.y = 0
+            self.vel_ang.linear.z = 0
+            self.vel_ang.angular.x = 0
+            self.vel_ang.angular.y = 0
+            self.vel_ang.angular.z = 0
+            try:
+                rate.sleep()
+            except rospy.ROSException:
+                quit()
+        for i in xrange(timeout * loop_freq):
+            self.vel_ang.linear.x = 5
+            self.vel_ang.linear.y = 0
+            self.vel_ang.linear.z = 0
+            self.vel_ang.angular.x = 0
+            self.vel_ang.angular.y = 0
+            self.vel_ang.angular.z = 0
+            try:
+                rate.sleep()
+            except rospy.ROSException:
+                quit()
+        for j in xrange(timeout-5 * loop_freq):
+            self.vel_ang.linear.x = 10
+            self.vel_ang.linear.y = 0
+            self.vel_ang.linear.z = 0
+            self.vel_ang.angular.x = 0
+            self.vel_ang.angular.y = 0.1
+            self.vel_ang.angular.z = 0
+            try:
+                rate.sleep()
+            except rospy.ROSException:
+                quit()
+if __name__ == '__main__':
+    rospy.init_node('controltest', anonymous=True)
+    systype = int(input("Input system type: \n"))
+    if systype == 1:
+        offboard_control = Ctrtest(systype)
+        offboard_control.test_posctl()
+    elif systype == 2 or systype == 3:
+        lat1, lon1 = input("WPT1 GPS coordinate : \n").split()
+        lat2, lon2 = input("WPT2 GPS coordinate : \n").split()
+        lat3, lon3 = input("WPT3 GPS coordinate : \n").split()
+        offboard_control = Ctrtest(systype, wp1lat=lat1, wp1lon=lon1, wp2lat=lat2, wp2lon=lon2)
+        if systype == 2:
+            offboard_control.test_attctl()
+        else:
+            offboard_control.test_vel()
+    elif systype == 4:
+        offboard_control = Ctrtest(systype)
+        offboard_control.test_vel_ang()
+    else:
+        print("wrong system type")
+        sys.exit(1)
+
--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# sAIM
-# 프로그램 폴더에 맞게 파일이나 폴더 만들어서 올려주시면 됩니다.
+# 자율주행 피자 배달 드론
+
+* 도심 내 작업 중인 사다리차를 회피하고, 아파트 베란다에 택배(피자)를 배달하기 위하 드론 개발 프로젝트입니다.
+* 제20회 로봇항공기 대회 자율주행 드론 부문 참가
+
+## 임무 개요
+1. 트럭 화물칸에서 드론이 이륙 후 WPT#1까지 이동
+2. WPT#2로 이동하며, 이동 간 임의로 배치된 사다리차를 인식 및 회피
+3. 아파트 3층을 인식하여 배란다 진입
+4. 3층 베란다 진입 후 택배(피자) 투하
+5. WPT#2 복귀 후 사다리차 재인식 및 회피
+6. WPT#1 도달 후 트럭 화물칸에 착륙
+<p align="center">
+    <img src="ASSET/Mission.png">
+</p>
+
+## 팀 구성
+<b> TEAM. sAIM</b>
+
+| 이름 | 담당 업무 |
+|-|-|
+|박진우| 팀장
+|금강산 | 기구 설계 |
+|정민기 | 기구 설계 |
+|장나경 | 기구 설계 |
+|이 수 | 제어SW 개발 |
+|박수현 | 제어SW 개발 |
+|박원우| 제어SW 개발 |
+|이용빈 | AI 개발 |
+|이재표 | AI 개발 |
+|정유찬 | AI 개발 |
+|유지원 | AI 개발 |
+|김민솔 | AI 개발 |
+
+
+## 사용 기술
+|개발 환경 | 사용 기술|
+|-|-|
+| OS | ![Ubuntu](https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white)|
+| 언어 | ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)  |
+| ML/DL | ![YOLO](https://img.shields.io/badge/yolo-111f68?style=for-the-badge&logo=yolo&logoColor=white) ![OpenCV](https://img.shields.io/badge/Opencv-5C3EE8?style=for-the-badge&logo=Opencv&logoColor=white)  |
+| 로보틱스 <br> 어플리케이션 | ![ROS2](https://img.shields.io/badge/ros2-22314E.svg?style=for-the-badge&logo=ros&logoColor=white) |
+| 프로젝트 관리도구| ![Notion](https://img.shields.io/badge/notion-000000.svg?style=for-the-badge&logo=notion&logoColor=white) |
+
+
+## 시스템 아키텍쳐
+<b> 드론 HW 레이아웃 
+</b>
+<p align="center">
+    <img src="ASSET/DroneHW.png">
+</p>
+
+<b> 시스템 아키텍처
+
+<p align="center">
+    <img src="ASSET/SysArch.png">
+</p>
+
+## 적용 기술
+* YOLO 기반 사다리차 인식
+
+<p align="center">
+    <img src="ASSET/OD.gif">
+</p>
+
+* Potential Field Method 기반 경로 설정 및 장애물 회피 알고리즘 적용
+
+<p align="center">
+    <img src="ASSET/PP.gif">
+</p>
+
+
+
+## 통합 기술 시연 영상
+* 사람 인식 (사다리차 인식 대체) 후 회피, 이동 경로 재설정
+
+<p align="center">
+    <img src="ASSET/OD_PP.gif">
+</p>
